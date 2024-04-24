e573731311b03944eda405efb8bb2ab85f8af16f
jtsiddons <jtsiddons@gmail.com>
2024-04-24 09:34:58 +0100

    fix: expose sub routines - match previous version

--- AirSeaFluxCode/__init__.py
+++ AirSeaFluxCode/__init__.py
@@ -1,10 +1,24 @@
 from .AirSeaFluxCode import AirSeaFluxCode
-from . import cs_wl_subs
-from . import flux_subs
-from . import hum_subs
-from . import util_subs
 
-__all__ = ['AirSeaFluxCode', 'cs_wl_subs',
-           'flux_subs', 'hum_subs', 'util_subs']
+from .cs_wl_subs import (cs, cs_Beljaars, cs_C35,
+                         cs_ecmwf, delta, get_dqer, wl_ecmwf)
+from .flux_subs import (cd_calc, cdn_calc, cdn_from_roughness, ctq_calc,
+                        ctqn_calc, get_LRb, get_Ltsrv, get_Rb, apply_GF,
+                        get_gust, get_strs, get_tsrv, get_stabco, psim_calc,
+                        psit_calc, psi_Bel, psi_ecmwf, psit_26, psi_conv,
+                        psi_stab, psim_ecmwf, psiu_26, psim_conv, psim_stab)
+from .hum_subs import gamma, get_hum, qsat_air, qsat_sea, VaporPressure
+from .util_subs import (CtoK, kappa, get_heights, gc,
+                        visc_air, set_flag, get_outvars, rho_air)
+
+__all__ = ['AirSeaFluxCode', 'cs', 'cs_Beljaars', 'cs_C35', 'cs_ecmwf',
+           'delta', 'get_dqer', 'wl_ecmwf', 'cd_calc', 'cdn_calc',
+           'cdn_from_roughness', 'ctq_calc', 'ctqn_calc', 'get_LRb',
+           'get_Ltsrv', 'get_Rb', 'apply_GF', 'get_gust', 'get_strs',
+           'get_tsrv', 'get_stabco', 'psim_calc', 'psit_calc', 'psi_Bel',
+           'psi_ecmwf', 'psit_26', 'psi_conv', 'psi_stab', 'psim_ecmwf',
+           'psiu_26', 'psim_conv', 'psim_stab', 'gamma', 'get_hum', 'qsat_air',
+           'qsat_sea', 'VaporPressure', 'CtoK', 'kappa', 'get_heights', 'gc',
+           'visc_air', 'set_flag', 'get_outvars', 'rho_air']
 
 __version__ = '1.2.0'
