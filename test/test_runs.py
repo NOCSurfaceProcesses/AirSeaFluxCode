c811b433af841a2f134c4cd1e1b3b7b3aaeea2b5
jtsiddons <jtsiddons@gmail.com>
2025-07-28 07:27:20 +0100

    test: add initial test to check that AirSeaFluxCode runs

---
+++ test/test_runs.py
@@ -0,0 +1,48 @@
+import pytest  # noqa: F401
+
+import os
+import numpy as np
+import pandas as pd
+
+import AirSeaFluxCode as asfc
+
+
+def test_toy_asfc() -> None:
+    # TEST: Simple test to check that the main process runs
+    data_path = os.path.join(asfc.__base__, "..", "Test_Data", "data_all.csv")
+    inDt = pd.read_csv(data_path)
+
+    date = np.asarray(inDt["Date"])
+    # lon = np.asarray(inDt["Longitude"])
+    lat = np.asarray(inDt["Latitude"])
+    spd = np.asarray(inDt["Wind speed"])
+    t = np.asarray(inDt["Air temperature"])
+    sst = np.asarray(inDt["SST"])
+    rh = np.asarray(inDt["RH"])
+    p = np.asarray(inDt["P"])
+    sw = np.asarray(inDt["Rs"])
+    hu = np.asarray(inDt["zu"])
+    ht = np.asarray(inDt["zt"])
+    hin = np.array([hu, ht, ht])
+    del hu, ht, inDt
+    outvar = ("tau", "sensible", "latent", "u10n", "t10n", "q10n")
+    # run AirSeaFluxCode
+    res = asfc.AirSeaFluxCode(
+        spd,
+        t,
+        sst,
+        "bulk",
+        meth="UA",
+        lat=lat,
+        hin=hin,
+        hum=["rh", rh],
+        P=p,
+        cskin=0,
+        Rs=sw,
+        tol=["all", 0.01, 0.01, 1e-05, 1e-3, 0.1, 0.1],
+        L="tsrv",
+        out_var=outvar,
+    )
+
+    assert isinstance(res, pd.DataFrame)
+    assert res.shape[0] == len(date)
